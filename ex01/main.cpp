3b67e42219ab326f441eb482e2488d3eedef73f5
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2026-03-19 21:19:28 +0100

    [ex01] main.cpp added, done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,6 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int main() {
+#include "RPN.hpp"
+#include <iostream>
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cerr << "Usage: ./RPN \"expression\"" << std::endl;
+		return 1;
+	}
 	
+	try {
+		RPN::evaluate(argv[1]);
+	} catch (const std::exception &e) {
+		std::cerr << "Error" << std::endl;
+		return 1;
+	}
+	return 0;
 }
